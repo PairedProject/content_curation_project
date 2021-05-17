a4473541eaa76da4bc3c18cd557c2d5996fbb991
Myles Traut <myles.traut1@gmail.com>
2021-05-17 19:02:37 +0200

    Created custom user model, register, login, logout and homepage

--- src/content_curation/settings.py
+++ src/content_curation/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # User defined apps
+    'users',
+    'pages'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'content_curation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +128,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
---
+++ src/users/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+
+	class Meta(UserCreationForm.Meta):
+		model = CustomUser
+		fields = ('username', 'email', 'subscribed')
+		help_texts = {
+			'username': None,
+			
+		}
+
+class CustomUserChangeForm(UserChangeForm):
+
+	class Meta(UserChangeForm.Meta):
+		model = CustomUser
+		fields = ('username', 'email', 'subscribed')
