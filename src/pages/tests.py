d52fe24b359f705d005d42fc95e7b074484d0592
Myles Traut <myles.traut1@gmail.com>
2021-05-18 10:13:55 +0200

    added tests for homepage/signup url, view, template and user signup form

--- src/pages/tests.py
+++ src/pages/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class HomePageTests(SimpleTestCase):
+
+	def test_home_page_status_code(self):
+		response = self.client.get('/')
+		self.assertEquals(response.status_code, 200)
+
+	def test_view_url_by_name(self):
+		response = self.client.get(reverse('home'))
+		self.assertEquals(response.status_code, 200)
+
+	def test_view_uses_correct_template(self):
+		response = self.client.get(reverse('home'))
+		self.assertEquals(response.status_code, 200)
+		self.assertTemplateUsed(response, 'home.html')
+
+class SignupPageTests(TestCase):
+
+	username = 'newuser'
+	email = 'newuser@gmail.com'
+
+	def sign_up_page_status_code(self):
+		response = self.client.get('/users/signup/')
+		self.assertEquals(response.status_code, 200)
+
+	
+
--- src/users/tests.py
+++ src/users/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+
+class SignupPageTests(TestCase):
+
+	username = 'newuser'
+	email = 'newuser@gmail.com'
+
+	def test_sign_up_page_status_code(self):
+		response = self.client.get('/users/signup/')
+		self.assertEquals(response.status_code, 200)
+
+	def test_view_url_by_name(self):
+		response = self.client.get(reverse('signup'))
+		self.assertEquals(response.status_code, 200)
+
+	def test_view_uses_correct_template(self):
+		response = self.client.get(reverse('signup'))
+		self.assertEquals(response.status_code, 200)
+		self.assertTemplateUsed(response, 'signup.html')
+
+	def test_user_signup_form(self):
+		new_user = get_user_model().objects.create_user(
+			username = self.username, 
+			email = self.email
+			)
+		self.assertEquals(get_user_model().objects.all().count(), 1)
+		self.assertEquals(get_user_model().objects.all()[0].username, self.username)
+		self.assertEquals(get_user_model().objects.all()[0].email, self.email)
+
