9b690ab017e7bf27145cd2aa6219e98695a97c23
Collins <coja1@outlook.com>
2021-06-17 20:14:15 +0200

    Selected to Scrape JSE using BeautifulSoup and Requests.

--- Website_Scrapping/scrap_JSE.py
+++ Website_Scrapping/scrap_JSE.py
@@ -1,17 +1,26 @@
-from bs4 import BeautifulSoup as soup #parsing HTML and XML documents
-import requests #for http requests
-from datetime import date
+from bs4 import BeautifulSoup
+import requests
 
-# getting current date information
-today = date.today()
-d = today.strftime("%Y-%m-%d")
-#print("date= ", d)
+base_url = 'https://www.jse.co.za'
 
-jse_url = "https://www.jse.co.za/"
-news = requests.get(jse_url)
-stock_news = soup(news.content,'lxml')
+source = requests.get(base_url).text
+print("Got source")
 
-for link in stock_news.findAll("main"):
-    print("page:{}".format(link.text))
+soup = BeautifulSoup(source, 'html.parser')
+print("Parsed source")
 
+articles = soup.find_all("article", class_="card")
+print(f"Number of articles found: {len(articles)}")
 
+for article in articles:
+    print("----------------------------------------------------")
+
+    headline = article.h3.text.strip()
+    link = base_url + article.a['href']
+    text = article.find("div", class_="field--type-text-with-summary").text.strip()
+    img_url = base_url + article.picture.img['data-src']
+
+    print(headline)
+    print(link)
+    print(text)
+    print("Image: "+ img_url)
