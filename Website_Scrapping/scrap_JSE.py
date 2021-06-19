2ce889406b0b3500481dbf31e6b578a2bd01563e
Collins <coja1@outlook.com>
2021-06-19 16:02:51 +0200

    Commit 4: Added Jason file and created dictionary

--- Website_Scrapping/scrap_JSE.py
+++ Website_Scrapping/scrap_JSE.py
@@ -1,5 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+
+
+
+top_stories = []
 
 base_url = 'https://www.jse.co.za'
 
@@ -8,19 +13,38 @@ print("Got source")
 
 soup = BeautifulSoup(source, 'html.parser')
 print("Parsed source")
-
 articles = soup.find_all("article", class_="card")
 print(f"Number of articles found: {len(articles)}")
 
 for article in articles:
-    print("----------------------------------------------------")
+    try:
+        headline = article.h3.text.strip()
+        link = base_url + article.a['href']
+        text = article.find("div", class_="field--type-text-with-summary").text.strip()
+        img_url = base_url + article.picture.img['data-src']
+        
+        print(headline,link,text,img_url)
+        
+        
+        stories_dict = {}
+        stories_dict['Headline'] = headline
+        stories_dict['Link'] = link
+        stories_dict['Text'] = text
+        stories_dict['Image'] = img_url
+        
+        top_stories.append(stories_dict)
+        
+    except AttributeError as ex:
+        print('Error:',ex)
+        
+with open('top_stories.json','w')as f:
+    json.dump(top_stories,f)
+    
+print('Fin.')
 
-    headline = article.h3.text.strip()
-    link = base_url + article.a['href']
-    text = article.find("div", class_="field--type-text-with-summary").text.strip()
-    img_url = base_url + article.picture.img['data-src']
-
-    print(headline)
-    print(link)
-    print(text)
-    print("Image: "+ img_url)
+        
+   
+      
+    
+    
+    
