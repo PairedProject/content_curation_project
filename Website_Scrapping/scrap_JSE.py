bd3f054f037f3bf2d7ec27afdc1aedc6f9f104ef
Collins <coja1@outlook.com>
2021-06-22 05:36:35 +0200

    Commit 5. Created dictionary to render to Django views for home page

--- Website_Scrapping/scrap_JSE.py
+++ Website_Scrapping/scrap_JSE.py
@@ -1,50 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
-import json
 
 
+def get_stories():
+    """user agent to facilitates end-user interaction with web content"""
 
-top_stories = []
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"
+    }
 
-base_url = 'https://www.jse.co.za'
+    top_stories = {"Headline": [], "Link": [], "Text": [], "Image": []}
+    base_url = "https://www.jse.co.za/"
 
-source = requests.get(base_url).text
-print("Got source")
+    source = requests.get(base_url, headers=headers).text
 
-soup = BeautifulSoup(source, 'html.parser')
-print("Parsed source")
-articles = soup.find_all("article", class_="card")
-print(f"Number of articles found: {len(articles)}")
+    soup = BeautifulSoup(source, "html.parser")
 
-for article in articles:
-    try:
-        headline = article.h3.text.strip()
-        link = base_url + article.a['href']
-        text = article.find("div", class_="field--type-text-with-summary").text.strip()
-        img_url = base_url + article.picture.img['data-src']
-        
-        print(headline,link,text,img_url)
-        
-        
-        stories_dict = {}
-        stories_dict['Headline'] = headline
-        stories_dict['Link'] = link
-        stories_dict['Text'] = text
-        stories_dict['Image'] = img_url
-        
-        top_stories.append(stories_dict)
-        
-    except AttributeError as ex:
-        print('Error:',ex)
-        
-with open('top_stories.json','w')as f:
-    json.dump(top_stories,f)
-    
-print('Fin.')
+    articles = soup.find_all("article", class_="card")
+    print(f"Number of articles found: {len(articles)}")
 
-        
-   
-      
-    
-    
-    
+    for article in articles:
+        try:
+            top_stories["Headline"].append(article.h3.text.strip())
+            top_stories["Link"].append(base_url + article.a["href"])
+            top_stories["Text"].append(
+                article.find("div", class_="field--type-text-with-summary").text.strip()
+            )
+            top_stories["Image"].append(base_url + article.picture.img["data-src"])
+
+        except AttributeError as ex:
+            print("Error:", ex)
+
+    #print(type(top_stories))
+    print(top_stories)
+
+
+get_stories()
