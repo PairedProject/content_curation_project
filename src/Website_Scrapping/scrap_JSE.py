df896c6e5de2c09e405e0d818f29c1ffdf4f1a3a
Collins <coja1@outlook.com>
2021-06-22 20:34:30 +0200

    Commit 6 Web scrapper.

--- src/Website_Scrapping/scrap_JSE.py
+++ src/Website_Scrapping/scrap_JSE.py
@@ -1,17 +1,38 @@
-from bs4 import BeautifulSoup as soup #parsing HTML and XML documents
-import requests #for http requests
-from datetime import date
-
-# getting current date information
-today = date.today()
-d = today.strftime("%Y-%m-%d")
-#print("date= ", d)
-
-jse_url = "https://www.jse.co.za/"
-news = requests.get(jse_url)
-stock_news = soup(news.content,'lxml')
-
-for link in stock_news.findAll("main"):
-    print("page:{}".format(link.text))
+from bs4 import BeautifulSoup
+import requests
 
 
+def get_stories():
+    """user agent to facilitates end-user interaction with web content"""
+
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"
+    }
+
+    top_stories = {"Headline": [], "Link": [], "Text": [], "Image": []}
+    base_url = "https://www.jse.co.za/"
+
+    source = requests.get(base_url, headers=headers).text
+
+    soup = BeautifulSoup(source, "html.parser")
+
+    articles = soup.find_all("article", class_="card")
+    print(f"Number of articles found: {len(articles)}")
+
+    for article in articles:
+        try:
+            top_stories["Headline"].append(article.h3.text.strip())
+            top_stories["Link"].append(base_url + article.a["href"])
+            top_stories["Text"].append(
+                article.find("div", class_="field--type-text-with-summary").text.strip()
+            )
+            top_stories["Image"].append(base_url + article.picture.img["data-src"])
+
+        except AttributeError as ex:
+            print("Error:", ex)
+
+    #print(type(top_stories))
+    print(top_stories)
+
+
+get_stories()
